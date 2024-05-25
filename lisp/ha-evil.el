7477b2a765533863b2b875dc2a4102f4b46eb6f6
hjoll6 <doolim98@gmail.com>
2024-05-25 18:47:36 +0900

    .

--- lisp/ha-evil.el
+++ lisp/ha-evil.el
@@ -1,5 +1,7 @@
 ;; ha-evil.el --- my evil configuration
 
+(use-package general :ensure t)
+
 (defun ha-evil/init()
   (setq evil-want-keybinding nil
 		evil-want-C-u-scroll t
@@ -11,7 +13,6 @@
 	  (define-key km (kbd k) nil))))
 
 
-;; (defvar-keymap ha-evil/leader-keymap :prefix)
 
 (use-package evil
   :ensure t
@@ -28,6 +29,12 @@
 		 ("SPC" . nil)
 		 ("SPC [" . #'previous-buffer)
 		 ("SPC ]" . #'next-buffer)
+		 ("SPC x" . #'execute-extended-command)
+		 ("SPC e f" . #'eval-defun)
+		 ("SPC e f" . #'eval-)
+		 ("SPC h v" . #'describe-variable)
+		 ("SPC h f" . #'describe-function)
+		 ("SPC h F" . #'describe-face)
 		 :map evil-normal-state-map
 		 ("SPC" . nil)
 		 ("SPC [" . #'previous-buffer)
@@ -36,45 +43,40 @@
 		 ("SPC F" . #'find-file-other-window)
 		 ("SPC b" . #'switch-to-buffer)
 		 ("SPC B" . #'switch-to-buffer)
-		 ("SPC h v" . #'switch-to-buffer)
 		 ("SPC SPC" . #'switch-to-buffer)
 		 ("M-." . nil)
 		 )
   :config
-  (evil-mode 0)
+  (evil-mode 1)
+  (add-hook 'special-mode-hook #'evil-motion-state)
   (global-set-key (kbd "<escape>") (kbd "C-g"))
+  (setq evil-motion-state-modes 
+		(append evil-emacs-state-modes evil-motion-state-modes))
+  (setq evil-emacs-state-modes nil)
   (ha-evil/keymap-unset
    (list evil-normal-state-map
 		 evil-insert-state-map
 		 evil-motion-state-map)
    (list "C-n" "C-p" "C-b" "C-f" "C-a" "C-e" "C-k"))
-  (use-package evil-collection
-	:after evil
-	:init
-	)
   (use-package evil-terminal-cursor-changer
 	:when (not (display-graphic-p))
 	)
-  (defface ha-evil/state-emacs-face
-	'((t (:background "Green" :foreground "Blue")))
-	"Evil Mode Emacs State Face")
+  )
 
-  (defface ha-evil/state-insert-face
-	'((t (:background "DodgerBlue1" :foreground "White")))
-	"Evil Mode Insert State Face")
+(defface ha-evil/state-emacs-face
+  '((t (:background "Green" :foreground "Blue")))
+  "Evil Mode Emacs State Face")
 
-  (defface ha-evil/state-normal-face
-	'((t (:background "Red" :foreground "White")))
-	"Evil Mode Normal Stace Face")
+(defface ha-evil/state-insert-face
+  '((t (:background "DodgerBlue1" :foreground "White")))
+  "Evil Mode Insert State Face")
 
-  (defface ha-evil/state-visual-face
-	'((t (:background "Purple" :foreground "White")))
-	"Evil Mode Normal Stace Face")
-  (use-package key-chord
-	:init
-	(key-chord-mode 1)
-	(key-chord-define evil-insert-state-map  "jk" 'evil-normal-state)
-	))
+(defface ha-evil/state-normal-face
+  '((t (:background "Red" :foreground "White")))
+  "Evil Mode Normal Stace Face")
 
+(defface ha-evil/state-visual-face
+  '((t (:background "Purple" :foreground "White")))
+  "Evil Mode Normal Stace Face")
 
 (provide 'ha-evil)
